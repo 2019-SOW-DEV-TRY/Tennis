f7dcca4e850b08d9f80a2c69708e6171e18d9f3a
2019-SOW-DEV-TRY <email@email.com>
2019-06-08 17:14:31 +0530

    Refactor - introduce Score enum to denote the different game scores possible

--- src/main/java/com/personal/kata/TennisGame.java
+++ src/main/java/com/personal/kata/TennisGame.java
@@ -6,8 +6,6 @@ public class TennisGame {
 
     public static final String HYPHEN = "-";
     public static final String SAME_GAME_SCORE = "All";
-    public static final String LOVE = "Love";
-    public static final String FIFTEEN = "Fifteen";
     private Player player1;
     private Player player2;
     private String gameScore;
@@ -15,7 +13,24 @@ public class TennisGame {
     public TennisGame() {
         player1 = new Player("Player 1");
         player2 = new Player("Player 2");
-        gameScore = LOVE + HYPHEN + SAME_GAME_SCORE;
+        gameScore = Score.LOVE.scoreCall + HYPHEN + SAME_GAME_SCORE;
+    }
+
+    private String calculateGameScore() {
+        String player1GameScore = Score.LOVE.scoreCall;
+        String player2GameScore = Score.LOVE.scoreCall;
+        if (isPlayerScoresEqual() && player1.getPlayerScore() == 0) {
+            gameScore = Score.LOVE.scoreCall + HYPHEN + SAME_GAME_SCORE;
+        } else if (isPlayerScoresEqual() && player1.getPlayerScore() == 1) {
+            gameScore = Score.FIFTEEN.scoreCall + HYPHEN + SAME_GAME_SCORE;
+        } else if (player1.getPlayerScore() == 1) {
+            player1GameScore = Score.FIFTEEN.scoreCall;
+            gameScore = player1GameScore + HYPHEN + player2GameScore;
+        } else if (player2.getPlayerScore() == 1) {
+            player2GameScore = Score.FIFTEEN.scoreCall;
+            gameScore = player1GameScore + HYPHEN + player2GameScore;
+        }
+        return gameScore;
     }
 
     public Player getPlayer1() {
@@ -30,21 +45,18 @@ public class TennisGame {
         return calculateGameScore();
     }
 
-    private String calculateGameScore() {
-        String player1GameScore = LOVE;
-        String player2GameScore = LOVE;
-        if (isPlayerScoresEqual() && player1.getPlayerScore() == 0) {
-            gameScore = LOVE + HYPHEN + SAME_GAME_SCORE;
-        } else if (isPlayerScoresEqual() && player1.getPlayerScore() == 1) {
-            gameScore = FIFTEEN + HYPHEN + SAME_GAME_SCORE;
-        } else if (player1.getPlayerScore() == 1) {
-            player1GameScore = FIFTEEN;
-            gameScore = player1GameScore + HYPHEN + player2GameScore;
-        } else if (player2.getPlayerScore() == 1) {
-            player2GameScore = FIFTEEN;
-            gameScore = player1GameScore + HYPHEN + player2GameScore;
+    private enum Score {
+        LOVE(0, "Love"),
+        FIFTEEN(1, "Fifteen"),
+        THIRTY(2, "Thirty");
+
+        private final int score;
+        private final String scoreCall;
+
+        Score(int score, String scoreCall) {
+            this.score = score;
+            this.scoreCall = scoreCall;
         }
-        return gameScore;
     }
 
     private boolean isPlayerScoresEqual() {
