cd617c155a73d687e89eba9c66c62aabb01c9ea0
2019-SOW-DEV-TRY <email@email.com>
2019-06-08 13:40:22 +0530

    Refactor - move game setup to @BeforeEach

--- src/test/java/com/personal/kata/TennisGameTest.java
+++ src/test/java/com/personal/kata/TennisGameTest.java
@@ -1,5 +1,6 @@
 package com.personal.kata;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TennisGameTest {
 
+    private TennisGame tennisGame;
+
+    @BeforeEach
+    public void newGameSetup() {
+        tennisGame = new TennisGame();
+    }
+
     @Test
     @DisplayName("Given a tennis game When tennis game starts Then there should be two players with scores at zero each")
     public void test_NewGameState_ShouldHaveTwoPlayerScores_ScoresAtZero() {
-
-        TennisGame tennisGame = new TennisGame();
-
         assertEquals(0, tennisGame.getPlayer1().getPlayerScore());
         assertEquals(0, tennisGame.getPlayer2().getPlayerScore());
     }
@@ -20,9 +25,6 @@ public class TennisGameTest {
     @Test
     @DisplayName("Given a tennis game When tennis game starts Then there should be two players named Player 1 and Player 2")
     public void test_NewGameState_ShouldHaveTwoPlayerNames_Player1_Player2() {
-
-        TennisGame tennisGame = new TennisGame();
-
         assertEquals("Player 1", tennisGame.getPlayer1().getPlayerName());
         assertEquals("Player 2", tennisGame.getPlayer2().getPlayerName());
     }
